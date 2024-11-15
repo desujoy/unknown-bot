9bd3815573cde50cb544f6d4444933c2dcd63134
Sujoy De <sujoydeofficial@gmail.com>
2024-11-15 22:56:06 +0530

    init with echo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	DISCORD_PUBLIC_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.post('/interactions', async (c) => {
+	if (c.req.method !== 'POST') {
+		return c.text('Method Not Allowed', { status: 405 });
+	}
+
+	const signature = c.req.header('X-Signature-Ed25519');
+	const timestamp = c.req.header('X-Signature-Timestamp');
+
+	if (!signature || !timestamp) {
+		return c.text('Unauthorized', { status: 401 });
+	}
+	const isValid = await verifyKey(await c.req.arrayBuffer(), signature, timestamp, c.env.DISCORD_PUBLIC_KEY);
+
+	if (!isValid) {
+		return c.text('Unauthorized', { status: 401 });
+	}
+
+	const interaction = await c.req.json();
+
+	if (interaction.type === InteractionType.PING) {
+		return c.json({ type: 1 });
+	}
+
+	if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+		const command = interaction.data.name;
+		const args = interaction.data.options || [];
+
+		console.log(args);
+
+		return c.json({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			data: {
+				content: command + ' ' + args.map((arg: any) => arg.value).join(' '),
+			},
+		});
+	}
+
+	return c.text('Bad Request', { status: 400 });
+});
+
+export default app;
